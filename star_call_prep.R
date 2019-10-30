fe69ff7b6f97d51d0c27e3cb306af2f8be128067
Kramer <evan.kramer@dc.gov>
2019-10-30 11:31:28 -0400

    added framework switch, student group, and metrics sections

--- star_call_prep.R
+++ star_call_prep.R
@@ -39,11 +39,19 @@ if(data) {
 
 # Analysis
 if(analysis) {
+  for(sch in sort(unique(star_scores$school_name))[1:3]) {
+  }
+  
   # LEA and school name
   n_distinct(star_scores$lea_code, star_scores$school_code)
   n_distinct(star_scores$school_name)
   
-  # STAR score change
+  # STAR score change (and relative performance)
+  transmute(
+    star_scores, 
+    school_name, 
+    pctile = round(100 * percent_rank(star_score_differences))
+  )
   
   # Number of frameworks
   frameworks = framework_scores$school_framework[framework_scores$school_name == sch]
@@ -76,11 +84,59 @@ if(analysis) {
       `Framework Points (Prior)` = x2018_framework_points_earned,
       `Framework Points Possible (Current)` = x2019_framework_points_possible,
       `Framework Points Possible (Prior)` = x2018_framework_points_possible
-    )
+    ) 
   
   # Student group performance
-  student_groups = student_group_scores$student_group[student_group_scores$school_name == sch]
-} else {
+  student_group_scores$student_group[student_group_scores$school_name == sch]
+  filter(student_group_scores, school_name == sch) %>% 
+    arrange(student_group_score_diff) %>%
+    transmute(
+      `School Name` = school_name, 
+      `Student Group` = student_group,
+      `Student Group Points (Current)` = x2019_student_group_points_earned,
+      `Student Group Points (Prior)` = x2018_student_group_points_earned,
+      `Student Group Points Possible (Current)` = x2019_student_group_points_possible,
+      `Student Group Points Possible (Prior)` = x2018_student_group_points_possible
+    ) %>% 
+    left_join(
+      group_by(student_group_scores, student_group) %>% 
+        summarize(`Student Group Points (Current State Average)` = mean(x2019_student_group_points_earned, na.rm = T)),
+      by = c("Student Group" = "student_group")
+    ) %>% 
+    select(`School Name`, `Student Group`, 
+           starts_with("Student Group Points ("), everything())
+  
+  # Metric performance
+  # All students metric scores
+  filter(metric_scores, str_detect(student_group, "All"), 
+         school_name == sch) %>% 
+    arrange(metric_points_difference) %>%
+    transmute(
+      `School Name` = school_name,
+      `Metric` = metric,
+      `Metric Points (Current)` = round(x2019_metric_points_earned, 2),
+      `Metric Points (Prior)` = x2018_metric_points_earned,
+      `Metric Points Possible (Current)` = x2019_metric_points_possible,
+      `Metric Points Possible (Prior)` = x2018_metric_points_possible
+    ) %>% 
+    left_join(
+      filter(metric_scores, student_group == "All Students") %>% 
+        group_by(metric) %>% 
+        summarize(`Metric Points (Current State Average)` = round(mean(x2019_metric_points_earned, na.rm = T), 2)),
+      by = c("Metric" = "metric")
+    ) %>% 
+    select(`School Name`, Metric, 
+           starts_with("Metric Points ("), everything())
+
+  # Subgroup with biggest change? 
+  group_by(student_group_scores, school_name) %>% 
+    filter(school_name == sch & student_group_score_diff == max(student_group_score_diff, na.rm = T)) %>% 
+    select(school_name, student_group) %>%
+    left_join(
+      metric_scores, 
+      by = c("school_name", "student_group")
+    )
+} else { 
   rm(analysis)
 }
 
@@ -101,53 +157,4 @@ if(output) {
 } else {
   rm(output)
 }
-
-
-
-
-
-
-# Which schools dropped most? Why?
-readxl::read_excel(
-  "Year over Year STAR Data for Analysis_v2 10.24.xlsx",
-  sheet = "STAR Scores"
-) %>% 
-  janitor::clean_names() %>% 
-  arrange(star_score_differences) %>% 
-  filter(row_number() <= 5) %>% 
-  select(
-    ends_with("_code"), ends_with("_name"), 
-    star_score_difference = star_score_differences,
-    star_score_2019 = x2019_star_score, 
-    star_score_2018 = x2018_star_score
-  ) %>% 
-  # List of schools that declined most 
-  print() %>% 
-  select(-ends_with("_name")) %>% 
-  left_join(
-    readxl::read_excel(
-      "Year over Year STAR Data for Analysis_v2 10.24.xlsx",
-      sheet = "STAR Student Group Scores"
-    ) %>% 
-      janitor::clean_names(),
-    by = c("lea_code", "school_code")
-  )
-
-# For the 5 biggest losers, which student groups and metrics dropped most? 
-# readxl::read_excel(
-#   "Year over Year STAR Data for Analysis_v2 10.24.xlsx",
-#   sheet = "STAR Scores"
-# ) %>% 
-#   janitor::clean_names() %>% 
-#   arrange(star_score_differences) %>% 
-#   filter(row_number() <= 5) %>% 
-#   left_join(
-#     readxl::read_excel(
-#       "Year over Year STAR Data for Analysis_v2 10.24.xlsx",
-#       sheet = "STAR Student Group Scores"
-#     ) %>% 
-#       janitor::clean_names(),
-#     by = c("lea_code", "school_code")
-#   ) %>% 
-#   arrange(student_group_score_diff) 
 
